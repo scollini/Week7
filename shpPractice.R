951587c92a0b667bfadcf06ea015a6791c3d161f
Sara Collini <scolli16@masonlive.gmu.edu>
2014-10-08 17:19:23 -0400

    Added the map code

--- shpPractice.R
+++ shpPractice.R
@@ -1,9 +1,3 @@
-install.packages(c("sp", "rgdal", "rgeos", "maptools",
-                   "ggmap", "ggplot2"),
-                 dependencies = TRUE)
-install.packages("rgdal", type = "source")
-install.packages("RColorBrewer")
-install.packages("classInt")
 library(sp)
 library(rgeos)
 library(maptools)
@@ -19,6 +13,8 @@ library(classInt)
 display.brewer.all()
 devtools::install_github("hadley/lazyeval")
 devtools::install_github("hadley/dplyr")
+
+pop_1840 <- read.csv("~/Desktop/Clio-3/Week7/nhgis0009_ds7_1840_state.csv")
 
 map_1840 <- readOGR("nhgis-shp/", "state_1840")
 
@@ -42,14 +38,16 @@ p <- ggplot() +
            map = map_1840_sp) +
   expand_limits(x = map_1840_sp$long, y=map_1840_sp$lat) +
   scale_fill_gradient2(low="white", high="red")
+
 print(p)
 
 p <- ggplot() + 
   geom_map(data = insane_1840, 
            aes(map_id = GISJOIN, fill = ColoredPublic + ColoredPrivate),
            map = map_1840_sp) +
-   expand_limits(x = map_1840_sp$long, y=map_1840_sp$lat) +
+   expand_limits(x = map_1840_sp$long, y=map_1840_sp$lat) 
   scale_fill_gradient2(low="white", high="red")
+
 print(p)
 
 p <- ggplot() + 
@@ -63,25 +61,19 @@ print(p)
 insane_merged <- map_1840_df %>%
 left_join(insane_1840, by = c("id" = "GISJOIN"))
 
-ggplot(data = insane_merged,
-       aes(x = long, y = lat, group = group, fill = WhitePublic, map_id = id)) +
-  geom_map(map = insane_merged, color = "gray", size = 0.2) + 
-  ggtitle("Number of Insane White Persons by Public Inst in 1840") + 
-  coord_map() +
-  theme_minimal() +
-  theme(legend.position = "bottom") +
-  scale_fill_brewer(palette = "YlGnBu") +
-  xlim(-98, -65)
-
 insane_merged_pop <- insane_merged %>%
   left_join(pop_1840, by = c("id" = "GISJOIN")) %>%
-  mutate(insane_merged, White = sum(WhitePublic + WhitePrivate)) %>%
-  mutate(insane_merged, NonWhite = sum(ColoredPublic + ColoredPrivate))
+  mutate(White = WhitePublic + WhitePrivate) %>%
+  mutate(NonWhite = ColoredPublic + ColoredPrivate) %>%
+  mutate(Total = White + NonWhite) %>%
+  mutate(percentpop = Total / ACD001) %>%
+ 
+insane_total_map <- ggplot() + 
+  geom_map(data = insane_merged_pop, 
+           aes(map_id = id, fill = percentpop),
+           map = map_1840_sp) +
+  expand_limits(x = map_1840_sp$long, y=map_1840_sp$lat) +
+  scale_fill_gradient2(low="white", high="red") 
 
-        
-           
-           
-
-
-
+print(insane_total_map)
 
